c4d00ed4ebc53e50876deeff6a97dc22dceb5123
Mason <1076536974@qq.com>
2021-02-22 00:41:22 +0800

    jwt 模拟登录，签发token 实现

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const Router = require('koa-router') // router
 const koaJwt = require('koa-jwt') // koa-jwt 用于校验token 等
 const Jwtoken = require('jsonwebtoken') // 用于生成JWT Token
 
+const app = new Koa()
+const router = new Router()
+app.use(koaCors())
+app.use(body())
+
 const SECRET = "2134124esadasdwacxz71290@7281983471……&*@#！……&@"
 
 const Options = {
@@ -13,10 +18,21 @@ const Options = {
     algorithm:"HS256"
 }
 
-const app = new Koa()
-const router = new Router()
-app.use(koaCors())
-app.use(body())
+// 模拟从数据库拿数据
+const fakeValid = {
+    user: "Mason",
+    password: "zhang123121@",
+    userId:"213123"
+}
+// 模拟校验函数
+const validUser = (username,password)=>{
+    if(username === fakeValid.user && password === fakeValid.password){
+        return true
+    }else{
+        return false
+    }
+}
+
 //401 用户没有权限，需要登录
 app.use(function(ctx, next){
     return next().catch((err) => {
@@ -35,22 +51,30 @@ app.use(koaJwt({secret:SECRET}).unless({path:['/login']}))
 
 router.post('/login',async (ctx)=>{
 
-    ctx.body = {
-        token:"hello!!world!!"
-    }
-    // 用户名 和密码 ，需要做校验 ，待留明天
-    const Payload = {
-        user:"admin",
-        userId:'21321'
-    }
-    // 生成 Jwt
-    const token = Jwtoken.sign(Payload,SECRET,Options)
-    console.log(token)
-    console.log(Jwtoken.decode(token))
-    // 将jwt Token 塞进 Authorization 里面
-    ctx.response.set('Authorization','Bearer '+ token)
 
     console.log(ctx.request.body)
+
+    const username = ctx.request.body?.username;
+    const password = ctx.request.body?.password;
+
+    if(validUser(username,password)){
+        const Payload = {
+            user:fakeValid.user,
+            userId:fakeValid.userId
+        }
+        // 生成 Jwt
+        const token = Jwtoken.sign(Payload,SECRET,Options)
+        // 尝试 token
+        console.log(token)
+        // 将jwt Token 塞进 Authorization 里面
+        ctx.response.set('Authorization','Bearer '+ token)
+        ctx.body={
+            message:"登录成功",
+            status:200
+        }
+    }else{
+        ctx.throw(403,"password False")
+    }
 })
 
 app.use(router.routes()).use(router.allowedMethods())
