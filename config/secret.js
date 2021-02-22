aa89ed42ea5f8e00e355c24468426305b6b29d6b
Mason <1076536974@qq.com>
2021-02-23 00:43:53 +0800

    抽取 密码模块

--- config/secret.js
+++ config/secret.js
@@ -1,4 +1,7 @@
-exports.secret = 'asdyuiqhjkdb21323@'
-exports.header = {
+exports.SECRET = "2134124esadasdwacxz71290@7281983471……&*@#！……&@"
 
+exports.Options = {
+    issuer:"www.biologyscience.cn",
+    expiresIn: '10m',
+    algorithm:"HS256"
 }
